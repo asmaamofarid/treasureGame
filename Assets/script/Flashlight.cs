7250dc74d4ff6d15d96c286f1021f26329d3adf4
faten <fatengamal16@gmail.com>
2018-04-25 13:38:42 +0200

    الجيم من الاول

---
+++ Assets/script/Flashlight.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Flashlight : MonoBehaviour
+{
+    public Light flashLight;
+    public AudioSource audioSource;
+
+    public AudioClip soundFlashlightOn;
+    public AudioClip soundFlashlightOff;
+
+    private bool isActive;
+
+    // Use this for initialization
+    void Start()
+    {
+        isActive = true;
+    }
+
+    // Update is called once per frame
+    // Use this for Input
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            if (isActive == false) //Toggle Flashlight On
+            {
+                flashLight.enabled = true;
+                isActive = true;
+
+                audioSource.PlayOneShot(soundFlashlightOn);
+            }
+            else if (isActive == true) //Toggle Flashlight Off
+            {
+                flashLight.enabled = false;
+                isActive = false;
+
+                audioSource.PlayOneShot(soundFlashlightOff);
+            }
+        }
+    }
+}
---
+++ Assets/script/Pistol.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Pistol : MonoBehaviour
+{
+    public int damage = 50;
+    public int ammo = 20;
+    //Optional
+    public float range = 50;
+
+    private Transform mainCamera;
+
+    private AudioSource myAudioSource;
+    public AudioClip shootSound;
+
+    // Use this for initialization
+    void Start()
+    {
+        myAudioSource = GetComponent<AudioSource>();
+        mainCamera = GameObject.FindGameObjectWithTag("MainCamera").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1") && ammo > 0)
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        //Raycast
+        Ray ray = new Ray(mainCamera.position, mainCamera.forward);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, range))
+        {
+            if (hit.collider.CompareTag("Enemy"))
+            {
+                //Damage the Enemy
+
+                Debug.Log("we hit an enemy , yaah");
+
+               //hit.collider.GetComponent<Enemy_Health>().TakeDamage(damage);
+            }
+        }
+
+          myAudioSource.PlayOneShot(shootSound);
+         ammo--;
+    }
+}
